cb12b5d508d39659828c71b2034b443b665ffbcc
ANNA <anna@baratey.ru>
2022-10-31 17:15:04 +0300

    Ex2

---
+++ Seminar1/Ex2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+System.Console.WriteLine("Введите три числа");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if ((num1>num2) && (num1>num3))
+{
+    Console.WriteLine($"Максимальное = {num1}");
+}
+else if((num2>num3) && (num2>num1))
+{
+   Console.WriteLine($"Максимальное = {num2}");
+}
+ else 
+{
+   Console.WriteLine($"Максимальное = {num3}");
+}
