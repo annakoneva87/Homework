ed1070834a1424fa17592fe96e940a8aaafc0462
ANNA <anna@baratey.ru>
2022-10-31 18:16:33 +0300

    Ex5

---
+++ Seminar2/Ex5/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+ 
+ System.Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int result=num%10;
+
+if ((num<100)||(num>999))
+{
+    Console.WriteLine($"ТРЕХЗНАЧНОЕ");
+}
+else
+{
+   Console.WriteLine($"{result}");   
+}
