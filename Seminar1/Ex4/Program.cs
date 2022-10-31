c1a01441e2ec3ba213b2fa0fee8a11934ef3665e
ANNA <anna@baratey.ru>
2022-10-31 17:22:02 +0300

    Ex4

---
+++ Seminar1/Ex4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
