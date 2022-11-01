72f7471bbb45423953e1dd49995cb27d4aa4e933
ANNA <anna@baratey.ru>
2022-11-01 12:06:09 +0300

    Ex6

---
+++ Seminar2/Ex6/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.  
+
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num<100)
+{
+   Console.WriteLine($" нет");
+}
+else
+while (num>1000)
+{
+    num/=10;
+int result = num % 10;
+   Console.WriteLine($" {result}");
+}
