56efba57b4769c7e01d55bf89c6c7627c50cee9b
ANNA <anna@baratey.ru>
2022-11-07 15:42:01 +0300

    Seminar3

---
+++ Seminar3/EX21/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, коорая принимает на вход число (а) и выдает сумму чисел от 1 до а
+
+int numA=SetNumber("A");
+
+int GetSumAllNumbersToNumberA(int numA);
+Print(sum);
+int SetNumber(string str)
+{
+    System.Console.WriteLine($"Enter number{str}");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int sum = GetSumAllNumbersToNumberA(numA A);
+{
+int sum=0;
+for (int i = 1; i < numA; i++)
+{
+    sum+=i;
+}
+return sum;
+{
+    
+}
+}
+
+
+void Print(int num)
+{
+System.Console.WriteLine($"number={num}");
+}
