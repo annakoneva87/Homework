3ca2d8eec5a2fc5c0c86a40b5f7b4fa63bbdca0b
ANNA <anna@baratey.ru>
2022-11-28 18:59:14 +0300

    Program

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,4 +1,6 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+﻿//Задача
+
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
